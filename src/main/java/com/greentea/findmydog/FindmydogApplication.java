02693600b42c9f862692aa3d81d9346679e7693b
GreenteaPIE <secret48156@gmail.com>
2024-04-14 19:35:45 +0900

    20240414 create Project

---
+++ src/main/java/com/greentea/findmydog/FindmydogApplication.java
@@ -0,0 +1,13 @@
+package com.greentea.findmydog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindmydogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindmydogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/greentea/findmydog/FindmydogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greentea.findmydog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindmydogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
