15e0be549ded6cabee546fee8b707c8043ad53c9
GreenteaPIE <secret48156@gmail.com>
2024-04-17 19:34:06 +0900

    update JPA update, insert 2404171933

--- src/main/java/com/greentea/findmydog/springboot/web/PostsApiController.java
+++ src/main/java/com/greentea/findmydog/springboot/web/PostsApiController.java
@@ -2,11 +2,11 @@ package com.greentea.findmydog.springboot.web;
 
 
 import com.greentea.findmydog.springboot.sevice.posts.PostsService;
+import com.greentea.findmydog.springboot.web.dto.PostsResponseDto;
 import com.greentea.findmydog.springboot.web.dto.PostsSaveRequestDto;
+import com.greentea.findmydog.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,5 +17,15 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
     }
 }
