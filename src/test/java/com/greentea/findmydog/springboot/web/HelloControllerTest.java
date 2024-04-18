addede73820b626c76faa72252c0186d40a16698
GreenteaPIE <secret48156@gmail.com>
2024-04-18 11:48:23 +0900

    update mustache template 2404181148

--- src/test/java/com/greentea/findmydog/springboot/web/HelloControllerTest.java
+++ src/test/java/com/greentea/findmydog/springboot/web/HelloControllerTest.java
@@ -1,6 +1,5 @@
 package com.greentea.findmydog.springboot.web;
 
-import com.greentea.findmydog.springboot.web.HelloController;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
