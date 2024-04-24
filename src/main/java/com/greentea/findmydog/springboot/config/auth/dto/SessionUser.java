a75c2b5558be7cde9a0a394693a5d8b61240724c
GreenteaPIE <secret48156@gmail.com>
2024-04-24 19:23:09 +0900

    update oauth setting 2404241922

---
+++ src/main/java/com/greentea/findmydog/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,17 @@
+package com.greentea.findmydog.springboot.config.auth.dto;
+
+import com.greentea.findmydog.springboot.domain.user.User;
+
+import java.io.Serializable;
+
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
