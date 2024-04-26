e11a0b8d4322d62d3c838032bfca25423420a202
GreenteaPIE <secret48156@gmail.com>
2024-04-26 20:41:53 +0900

    update spring session jdbc 2404261041

--- src/main/java/com/greentea/findmydog/springboot/web/IndexController.java
+++ src/main/java/com/greentea/findmydog/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.greentea.findmydog.springboot.web;
 
+import com.greentea.findmydog.springboot.config.auth.LoginUser;
 import com.greentea.findmydog.springboot.config.auth.dto.SessionUser;
 import com.greentea.findmydog.springboot.sevice.posts.PostsService;
 import com.greentea.findmydog.springboot.web.dto.PostsResponseDto;
@@ -18,9 +19,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user != null) {
             model.addAttribute("userName", user.getName());
         }
