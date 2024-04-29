34053dc8fc4178e03fbff9e30ffc02a35f4d5cfb
GreenteaPIE <secret48156@gmail.com>
2024-04-29 22:19:26 +0900

    update Spring boot security test 2404292219

--- src/main/java/com/greentea/findmydog/springboot/Application.java
+++ src/main/java/com/greentea/findmydog/springboot/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+// @EnableJpaAuditing Test오류로 인해 삭제
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
