7fb9fc9798f64531ad569bdf85a3f095fb86cf6d
GreenteaPIE <secret48156@gmail.com>
2024-05-10 19:58:27 +0900

    update Spring boot fix header ui 2405101958

--- src/main/java/com/greentea/findmydog/springboot/sevice/posts/PostsService.java
+++ src/main/java/com/greentea/findmydog/springboot/sevice/posts/PostsService.java
@@ -92,7 +92,6 @@ public class PostsService {
     public void delete(Long id){
         Posts posts = postsRepository.findById(id)
                 .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
-
         postsRepository.delete(posts);
     }
 }
