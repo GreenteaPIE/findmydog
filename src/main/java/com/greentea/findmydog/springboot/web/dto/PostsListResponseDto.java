316dc47f6bd9fd881a723a9edbe6dfd7750fd0cc
GreenteaPIE <secret48156@gmail.com>
2024-05-11 18:13:43 +0900

    update Spring boot fix slick slider 2405111813

--- src/main/java/com/greentea/findmydog/springboot/web/dto/PostsListResponseDto.java
+++ src/main/java/com/greentea/findmydog/springboot/web/dto/PostsListResponseDto.java
@@ -4,12 +4,15 @@ import com.greentea.findmydog.springboot.domain.posts.Posts;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 public class PostsListResponseDto {
     private Long id;
     private String title;
     private String author;
+    private List<ImageDto> images;
     private LocalDateTime modifiedDate;
 
     public PostsListResponseDto(Posts entity){
@@ -17,5 +20,8 @@ public class PostsListResponseDto {
         this.title = entity.getTitle();
         this.author = entity.getAuthor();
         this.modifiedDate = entity.getModifiedDate();
+        this.images = entity.getImages().stream()
+                .map(image -> new ImageDto(image.getId(), image.getOriginalFileName(), image.getStoredFileName()))
+                .collect(Collectors.toList());
     }
 }
